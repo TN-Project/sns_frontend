39070c468e74519dc90ccd1c03115bb7f4bf8ebb
natyosu3 <nachi334825@gmail.com>
2024-06-19 11:40:02 +0900

    バリデーション・リダイレクト抜きでログイン機能を実装

--- react_sns/src/features/loginPage/loginPage.tsx
+++ react_sns/src/features/loginPage/loginPage.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const login = () => {
+export const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include'  
+    });
+
+    if (response.ok) {
+      // タイムラインページにリダイレクト
+    } else {
+      alert("ログインに失敗しました")
+    }
+  }
+
   return (
-    <div>login</div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   )
 }

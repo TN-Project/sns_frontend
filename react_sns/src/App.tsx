81afa2514b880ce1917bbc8c623e40f9b4f02f6e
ts5354 <tsunodashuma@gmail.com>
2024-06-20 15:28:41 +0900

    a

--- react_sns/src/App.tsx
+++ react_sns/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import New_picture from "./features/new_picture/New_picture"
 import { LoginPage } from './features/loginPage/loginPage';
+import {SignUpPage} from './features/signup/signup';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
     <Route path="/picture" element={<New_picture />} />
     <Route path="/login" element={<LoginPage />} />
+    <Route path="/signup" element={<SignUpPage />} />
   </Routes>
   );
 }
---
+++ react_sns/src/features/signup/signup.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import './signup.css'
+
+export const SignUpPage = () => {
+  const navigate = useNavigate();
+  const [nickname, setNickname] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [retypePassword, setRetypePassword] = useState('');
+
+  const [cookies] = useCookies(['login']);
+  useEffect(() => {
+    // クッキーの値を取得する
+    const sessionCookie = cookies.login;
+
+    if (sessionCookie) {
+      // ログインしている場合はタイムラインページにリダイレクト
+      navigate('/');
+    }
+  }, [cookies]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // パスワードと再入力パスワードが一致しているかチェック
+    if (password !== retypePassword) {
+      alert("パスワードが一致しません");
+      return;
+    }
+
+    // 各入力値が空でないかチェック
+    if (nickname === '' || username === '' || password === '') {
+      alert("入力値が空です");
+      return;
+    }
+
+    // ユーザー登録APIを呼び出す
+    const response = await fetch('http://localhost:8080/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include'
+    });
+
+    if (response.ok) {
+      // タイムラインページにリダイレクト
+      navigate('/');
+    } else {
+      alert("アカウント登録に失敗しました\nhint: すでに登録されているユーザー名かもしれません")
+    }
+  }
+
+  return (
+    <>
+    <div className='signup-content'>
+    <form onSubmit={handleSubmit}>
+      <h1>新規アカウント登録</h1>
+        <label>
+          Nickname:
+          <input type="text" value={nickname} onChange={e => setNickname(e.target.value)} />
+        </label>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <label>
+          【確認】ReType Password:
+          <input type="password" value={retypePassword} onChange={e => setRetypePassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+    </>
+  )
+}

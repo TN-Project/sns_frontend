7941ba105cd1d099b9adecbbf496dd5c12963029
ts5354 <tsunodashuma@gmail.com>
2024-06-20 18:49:15 +0900

    写真投稿機能とReactRouterのセットアップ

--- react_sns/src/App.tsx
+++ react_sns/src/App.tsx
@@ -4,13 +4,16 @@ import './App.css';
 import New_picture from "./features/new_picture/New_picture"
 import { LoginPage } from './features/loginPage/loginPage';
 import {SignUpPage} from './features/signup/signup';
+import {AddGroupPage} from './features/new_group/new_group';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Routes>
+  <Routes>
     <Route path="/picture" element={<New_picture />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/signup" element={<SignUpPage />} />
+    <Route path="/new_group" element={<AddGroupPage/>} />
+    
   </Routes>
   );
 }
--- react_sns/src/features/new_picture/components/Group_Select.tsx
+++ react_sns/src/features/new_picture/components/Group_Select.tsx
@@ -1,21 +1,42 @@
 import React,{useEffect,useState}from 'react'
-
+import "./Group_Select.css"
+import { useNavigate } from 'react-router-dom';
 const Group_Select= () => {
-    const [group_list,setGroup_list]=useState([])
+    const [group_list,setGroup_list]=useState<string[]>([])
     const [group,setGroup]=useState("")
     const [image, setImage] = useState<File>()
+    const [texterr,setTexterr]=useState<boolean>(false)
+    const navigate = useNavigate();
     const fileChange = (event: React.FormEvent) => {
         const files = (event.target as HTMLInputElement).files
-    
         if (files && files.length > 0) {
             setImage(files[0])
         }
     }
+    interface Group {
+      Group_id: number;
+      Group_name: string;
+    }
+    
+    interface ResponseData {
+      groups: Group[];
+      message: string;
+    }
+    const extractGroupNames = (data: ResponseData): string[] => {
+     return data.groups.map(group => group.Group_name);
+    }
+    
     async function fetchData() {
         try {
-          const response = await fetch('http://localhost:8080/group/acquisition-affiliation-user');
-          const data = await response.json();
-          setGroup_list(data)
+          const response = await fetch('http://localhost:8080/group/acquisition-affiliation-user',{
+            credentials: "include",
+          });
+          const data:ResponseData= await response.json();
+          
+          const group_data=extractGroupNames(data);
+          
+          setGroup_list(group_data)
+          
         } catch (error) {
           console.error('リクエストエラー:', error);
         }
@@ -26,46 +47,47 @@ const Group_Select= () => {
     const handleSubmit = async (e: React.FormEvent) => {
       e.preventDefault(); 
       const formData = new FormData();
-      if (!image) 
-        return;
+      if (!image){
+        setTexterr(true);
+      }else{
       formData.append('file', image);
       formData.append('group_name', group);
       try{
       const response = await fetch('http://localhost:8080/picture/upload', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
         body: formData,
-         
+        credentials: 'include'   
       });
       if (response.ok) {
         console.log('Upload successful');
+        navigate('/');
       } else {
         console.error('Upload failed');
+        
+        
       }
     } catch (error) {
       console.error('リクエストエラー:', error);
     }
     }
-  
+  }
+    useEffect(() => {
+      console.log(group)
+    },[group]);
   return (
     <div>
+      {texterr && <p style={{ color: 'red' }}>エラーが発生しました。</p>}
       <form>
-        {
-          (function () {
-            const list = [];
-            for (const i in group_list) {
-              list.push(<option value={i}>{i}</option>);
-            }
-            return <select value={group} onChange={e => setGroup(e.target.value)}>{list}</select>;
-          }())
-        }
-      
         
+          <p>グループを選択</p>
+          <select value={group} onChange={e => setGroup(e.target.value)}>
+            {group_list.map((group_name, index) => (
+            <option key={index} value={group_name}>{group_name}</option>
+          ))}
+          </select>
         <input type="file" onChange={fileChange}/>
-        <button type="submit" onClick={handleSubmit}></button>
-        </form>
+        <button type="submit" onClick={handleSubmit}>アップロード</button>
+      </form>
     </div>
    
   )

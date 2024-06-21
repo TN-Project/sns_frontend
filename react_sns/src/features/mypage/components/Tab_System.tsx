9ea99bbd31cbd7231ef36c6fc4430e1ee4a6bec2
ts5354 <tsunodashuma@gmail.com>
2024-06-21 21:43:09 +0900

    pull

--- react_sns/src/features/mypage/components/Tab_System.tsx
+++ react_sns/src/features/mypage/components/Tab_System.tsx
@@ -3,6 +3,13 @@ import React,{useEffect,useState}from 'react'
 import 'react-tabs/style/react-tabs.css';
 const Tab_System = () => {
   const [group_list,setGroup_list]=useState<string[]>([])
+  const [group_id,setGroup_id]=useState<number[]>([])
+  const [group_picture,setGroup_picture]=useState<GroupPicture>()
+  const [rpdata,setRpdata]=useState<ResponseData>()
+  interface GroupPicture {
+    Group_id: number;
+    Picture_id: string[];
+  }
   interface Group {
     Group_id: number;
     Group_name: string;
@@ -12,33 +19,67 @@ const Tab_System = () => {
     groups: Group[];
     message: string;
   }
-    const extractGroupNames = (data: ResponseData): string[] => {
+  /*const extractGroupNames = (data: ResponseData): string[] => {
     return data.groups.map(group => group.Group_name);
-   }
+  }*/
+  const extractGroupId = (data: ResponseData): number[] => {
+    return data.groups.map(group => group.Group_id);
+  }
+
   async function fetchData() {
     try {
       const response = await fetch('http://localhost:8080/group/acquisition-affiliation-user',{
         credentials: "include",
       });
       const data:ResponseData= await response.json();
+      console.log(data);
+      setRpdata(data)
+      /*グループ名を取得*/
+      /*const group_data=extractGroupNames(data);
+      setGroup_list(group_data)*/
+       /*グループidを取得*/
+      const group_id_list=extractGroupId(data);
+      setGroup_id(group_id_list)
       
-      const group_data=extractGroupNames(data);
       
-      setGroup_list(group_data)
-      
+    } catch (error) {
+      console.error('リクエストエラー:', error);
+    }
+  }
+  const IdToName=(value:Number)=>{
+    if (rpdata){
+      const group = rpdata.groups.find(group => group.Group_id === value);
+      return group && group.Group_name ;
+    } 
+  }
+  
+  async function fetchPicture(value:number) {
+    try {
+          const response =await fetch('/group/{value}/pictures-list',{
+          credentials: "include",
+          });
+          const data:GroupPicture["Picture_id"]= await response.json();
+          const group_picture:GroupPicture={
+            Group_id: value,
+            Picture_id:data
+          }
+          setGroup_picture(group_picture)  
     } catch (error) {
       console.error('リクエストエラー:', error);
     }
   }
 useEffect(() => {
     fetchData();
+    /*fetchPicture();*/
 },[]);
     return (
       
         <Tabs>
           <TabList>
-          {group_list.map((group_name, index) => (
-          <Tab key={index} value={group_name}>{group_name}</Tab>
+          {group_id.map((id, index) => (
+          <Tab key={index} value={id} /*onClick={fetchPicture()}*/>
+            {IdToName(id)}
+          </Tab>
         ))}
   
             

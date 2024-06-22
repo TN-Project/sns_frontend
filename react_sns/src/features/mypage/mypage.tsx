dd7bb019e7e09eba770d9ee619982c75507d8783
ts5354 <tsunodashuma@gmail.com>
2024-06-22 23:19:40 +0900

    ログアウト以外実装

--- react_sns/src/features/mypage/mypage.tsx
+++ react_sns/src/features/mypage/mypage.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from 'react';
 import Tab_System from './components/Tab_System';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import "./mypage.css"
+import './mypage.css';
 
 const Mypage = () => {
   const [cookies] = useCookies(['login']);
   const sessionCookie = cookies.login;
   const navigate = useNavigate();
   
-const MakeGroup=()=>{
-  navigate("/new_group");
-}
-const UpPicture=()=>{
-  navigate("/picture");
-}
-  return (
-
-   <>
-   
-   <button className='makegroup' type="button" onClick={MakeGroup}>グループ新規作成</button>
-   
+  const MakeGroup = () => {
+    navigate('/new_group');
+  };
   
-  <button className='uppicture' type="button" onClick={UpPicture}>写真を投稿する</button>
-    <Tab_System/>
-  </>
-  )
+  const UpPicture = () => {
+    navigate('/picture');
+  };
+
+  return (
+    <div className="mypage">
+      <div className="button-container">
+        <button className='makegroup' type="button" onClick={MakeGroup}>グループ新規作成</button>
+        <button className='uppicture' type="button" onClick={UpPicture}>写真を投稿する</button>
+      </div>
+      <Tab_System />
+    </div>
+  );
 }
 
-export default Mypage
+export default Mypage;
